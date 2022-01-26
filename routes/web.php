ea450c721aa77543dc4890d77dba4cba6835eaae
Rebjai <rebjai@github.com>
2022-01-26 10:45:14 -0600

    import controller class

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ResponsesController;
 use App\Http\Controllers\Alumno\EstudioController;
 use App\Http\Controllers\Bachillerato\areaController;
 use App\Http\Controllers\Bachillerato\plantelController;
