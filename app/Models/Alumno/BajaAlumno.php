fa559346a555070938646192fd5ee01e35d32fa4
Rebjai <rebjai@github.com>
2022-02-01 09:36:17 -0600

    adding causas baja relation

--- app/Models/Alumno/BajaAlumno.php
+++ app/Models/Alumno/BajaAlumno.php
@@ -2,11 +2,23 @@
 
 namespace App\Models\Alumno;
 
+use App\Models\Cuestionario\CausaBaja;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BajaAlumno extends Model
 {
     use HasFactory;
     protected $fillable = ['causa_baja_id', 'apoyo_economico'];
+    
+    /**
+     * Get the causa_baja that owns the BajaAlumno
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function causa_baja(): BelongsTo
+    {
+        return $this->belongsTo(CausaBaja::class);
+    }
 }
