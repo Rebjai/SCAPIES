5eadcc82b33f4562aeb22ef19bdf416e79154712
Rebjai <rebjai@github.com>
2022-02-01 09:37:17 -0600

    adding dato academico relations

--- app/Models/Alumno/DatoAcademico.php
+++ app/Models/Alumno/DatoAcademico.php
@@ -2,12 +2,34 @@
 
 namespace App\Models\Alumno;
 
+use App\Models\Bachillerato\Area;
+use App\Models\Bachillerato\Plantel;
+use App\Models\Bachillerato\Subsistema;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DatoAcademico extends Model
 {
     use HasFactory;
     protected $table = 'datos_academicos';
     protected $guarded = [];
+
+    /**
+     * Get the subsistema that owns the DatoAcademico
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function subsistema(): BelongsTo
+    {
+        return $this->belongsTo(Subsistema::class);
+    }
+    public function plantel(): BelongsTo
+    {
+        return $this->belongsTo(Plantel::class);
+    }
+    public function campo_formacion(): BelongsTo
+    {
+        return $this->belongsTo(Area::class);
+    }
 }
